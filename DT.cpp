9c6f997c840eb696fe732629e981fa6ec64582eb
Fernando Arriaga <a01270913@itesm.mx>
2018-10-23 13:53:53 -0500

    Update DT.cpp

--- DT.cpp
+++ DT.cpp
@@ -7,12 +7,63 @@
 #include<cmath>
 #include<iomanip>
 class attribute;
+class attributenode;
+class Valuenode;
+class resultnode;
+
+class attributenode {
+public:
+std::string Name;
+std::vector<Valuenode*> MyValues;
+Valuenode* Parentvalue;
+float Myentropy;
+friend class Valuenode;
+};
+
+class Valuenode {
+public:
+std::string Name;
+attributenode* Nextattribute;
+attributenode* Myattribute;
+std::string Myresult;
+int mysize;
+float myentropy;
+friend class attributenode;
+
+};
+
+bool isvaluepresent(Valuenode* val2)
+{
+//if value node is present on the line of data return true
+}
+bool findresults(Valuenode* val1)
+{
+//for every parent value check if it's present, use recursion.
+}
+int numofresults(Valuenode* val, std::string res)
+{ int number=0;
+//for every line of data
+//if the result is there,
+//if res==res of that line add 1 to number
+return number;
+}
+
+float entropy (Valuenode* VN)
+{
+//for every possible result,do the algorithm based on number of(result)
+}
+
+float infoGain(Valuenode*parentv, attributenode*expanded)
+{
+//calculate using the parentv entropy and the entropy of the values of the expanded attribute.
+}
 
 class attribute{
 	public:
 		std::string name;
 		std::vector<std::string> values;
 };
+
 void read_data(){
 	attribute att;
 	std::string line;
