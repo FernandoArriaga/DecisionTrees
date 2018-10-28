fc4fe6b18b0d005752b61fed2af866ff3f79dfca
Fernando Arriaga <a01270913@itesm.mx>
2018-10-28 11:40:36 -0600

    Update DT.cpp

--- DT.cpp
+++ DT.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -16,6 +17,8 @@ std::vector<attributenode*>attributes;
 int attindex=0;
 attributenode* Fresult;
 bool fdata=0;
+float Intrpy;
+attributenode* Head;
 
 class attributenode {
 public:
@@ -24,9 +27,12 @@ std::string Name;
 std::vector<Valuenode*> MyValues;
 Valuenode* Parentvalue;
 float Myentropy;
+attributenode();
 friend class Valuenode;
 };
-
+attributenode::attributenode(void){
+Parentvalue=0;
+}
 class Valuenode {
 public:
 std::string Name;
@@ -44,33 +50,108 @@ std::vector<std::string> features;
 std::string label;
 };
 
-bool isvaluepresent(Valuenode* val2)
+bool isvaluepresent(Valuenode* val2,Data* D)
 {
 //if value node is present on the line of data return true
+if(val2->Name.compare(D->features[val2->Myattribute->Myindex])==0)
+    return true;
+return false;
 }
-bool findresults(Valuenode* val1)
+bool findresults(Valuenode* val1,Data* D1)
 {
 //for every parent value check if it's present, use recursion.
+if(val1->Myattribute->Parentvalue!=0)
+    return (isvaluepresent(val1,D1))&&(findresults(val1->Myattribute->Parentvalue,D1));
+return isvaluepresent(val1,D1);
 }
-int numofresults(Valuenode* val, std::string res)
-{ int number=0;
-//for every line of data
-//if the result is there,
-//if res==res of that line add 1 to number
-return number;
-}
+
 
 float entropy (Valuenode* VN)
 {
 //for every possible result,do the algorithm based on number of(result)
+float Nres=0;
+    float ntrpy=0;
+    float dsize;
+    float p=0;
+for(int i=0;i<Fresult->MyValues.size();i++)
+{   VN->mysize=0;
+    for(int j=0;j<Dset.size();j++)
+    {
+        if(findresults(VN,Dset[j]))
+        {
+            VN->mysize+=1;
+            if(Dset[j]->label.compare(Fresult->MyValues[i]->Name)==0)
+            Nres+=1;
+        }
+
+    }
+
+    if(Nres>0)
+    {
+    dsize=VN->mysize;
+    p=Nres/dsize;
+    ntrpy+=-1*(p)* log2 (p);
+    if(ntrpy==0)
+        VN->Myresult+=Fresult->MyValues[i]->Name;
+    }
+    Nres=0;
+
+}
+VN->myentropy=ntrpy;
+return ntrpy;
 }
 
 float infoGain(Valuenode*parentv, attributenode*expanded)
 {
 //calculate using the parentv entropy and the entropy of the values of the expanded attribute.
 }
-float initentropy(void){
 
+attributenode* inatt(void)
+{
+    float ntrpy=0;
+    float inG=Intrpy;
+    float maxinG=0;
+    attributenode* p;
+for(int i=0;i<attributes.size();i++)
+    {
+        for(int j=0;j<attributes[i]->MyValues.size();j++)
+        {
+            ntrpy=entropy(attributes[i]->MyValues[j]);
+            inG-=(attributes[i]->MyValues[j]->mysize/Dset.size())*ntrpy;
+        }
+        if(inG>maxinG)
+            {
+            maxinG=inG;
+            p=attributes[i];
+            }
+    }
+    return p;
+}
+
+
+float initentropy(void){
+    float Nres=0;
+    float ntrpy=0;
+    float dsize;
+    float p=0;
+for(int i=0;i<Fresult->MyValues.size();i++)
+{
+    for(int j=0;j<Dset.size();j++)
+    {
+        if(!Fresult->MyValues[i]->Name.compare(Dset[j]->label))
+            Nres+=1;
+    }
+
+    if(Nres>0)
+    {
+    dsize=Dset.size();
+    p=Nres/dsize;
+    ntrpy+=-1*(p)* log2 (p);
+    }
+    Nres=0;
+
+}
+return ntrpy;
 }
 class attribute{
 	public:
@@ -87,7 +168,7 @@ void read_data(){
 			std::getline(std::cin, line);
 		if(line.find("@relation") != -1){
 			std::string relation = line.substr(line.find(" ")+1);
-			std::cout << relation;
+			//std::cout << relation;
 			std::getline(std::cin, line);
 		}
 		if(line.find("@attribute") != -1){
@@ -98,12 +179,12 @@ void read_data(){
 			buff=line.substr(line.find(" ")+1);
 			buff2=buff.substr(0,buff.find(" "));
 			if(buff2.find("	")!=-1)
-           			 {
-           			 buff2=buff.substr(0,buff.find("	"));
-           			 }
+            {
+            buff2=buff.substr(0,buff.find("	"));
+            }
 			//To do
 			att->Name+=(buff2);
-			std::cout<<att->Name;
+			//std::cout<<att->Name;
 			att->Myindex=attindex;
 			attindex+=1;
 			//std::cout<<att->Myindex;
@@ -141,6 +222,21 @@ void read_data(){
             fdata=1;
             //std::getline(std::cin, line);
             }
+        else if (fdata==1){
+            Data* D1=new Data;
+            std::string tokend;
+            std::string delimiterd = ",";
+            size_t posd = 0;
+            while ((posd = line.find(delimiterd)) != std::string::npos) {
+    			tokend = line.substr(0, posd);
+    			D1->features.push_back(tokend);
+    			//std::cout << Van->Name << std::endl;
+    			line.erase(0, posd + delimiterd.length());
+			}
+			D1->label.clear();
+			D1->label+=line;
+			Dset.push_back(D1);
+        }
 std::getline(std::cin, line);
 
 }
@@ -150,6 +246,15 @@ int main(){
     while(!fdata)
     {
 	read_data();
-	std::cout<<fdata;
     }
+    Fresult=attributes[(attributes.size()-1)];
+    attributes.pop_back();
+    //std::cout<<Dset[1]->features[1];
+    //std::cout<<attributes.size()<<std::endl;
+    //std::cout<<attributes[(attributes.size()-1)]->Name<<std::endl;
+    //std::cout<<Fresult->Name;
+    Intrpy=initentropy();
+    std::cout<<Intrpy;
+    Head=inatt();
+    std::cout<<Head->Name<<Head->MyValues[0]->Myresult;
 }
